d1626a831c1dd829c1a7d9d118351a6041c3c784
Gabriela Guanini <gabriela.guanini@gmail.com>
2022-07-23 19:35:55 -0300

    Primera Masterclass, se agrego el proyecto de INITIALIAZR con la práctica de los CRUD con una ArrayList, se agregan los CRUD Editar y Borrar como propone la profesora

---
+++ BackEnd/Mod 8 SpringBoot/SpringBoot/src/main/java/com/ejemplo/SpringBoot/controller/Controller.java
@@ -0,0 +1,70 @@
+
+package com.ejemplo.SpringBoot.controller;
+
+import com.ejemplo.SpringBoot.model.Persona;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    
+    List <Persona> listaPersonas = new ArrayList();
+    
+    @GetMapping("/hola/{nombre}")
+    public String decirHola(@PathVariable String nombre){
+    return "Hola Mundo" + nombre;
+    }
+    
+    @GetMapping("/chau")
+    public String decirChau(@RequestParam String nombre, @RequestParam String apellido, @RequestParam int edad){
+    return "Chau Mundo" + " Nombre: " + nombre + " Apellido: " + apellido + " Edad: "+ edad;
+    }
+    
+    @PostMapping("/new/persona")
+    public void agregarPersona(@RequestBody Persona pers){
+        listaPersonas.add(pers);
+    }
+    
+    @GetMapping("/ver/personas")
+    @ResponseBody
+    public List <Persona> verPersonas(){
+    return listaPersonas;
+    }
+    
+  
+@DeleteMapping ("/borrar")
+public void borrarPersona(@RequestParam Long posicion){
+
+    
+   int miPosicionInt= Math.toIntExact(posicion);
+    
+ listaPersonas.remove(miPosicionInt);
+  
+}
+
+
+
+@PutMapping ("/editar")
+public void editarPersona(@RequestParam Long posicion, @RequestBody Persona pers){
+
+ int miPosicionInt2= Math.toIntExact(posicion);    
+    
+listaPersonas.set(miPosicionInt2, pers);
+ 
+ 
+}
+
+
+}
+
+
